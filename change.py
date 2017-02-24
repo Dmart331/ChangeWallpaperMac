b9d1ec073ab6ae4abfd9e97421979d5d33eadede
Paul Martin <Dmart331@gmail.com>
2017-02-24 10:14:25 -0600

    Cleaned up code. added comments

--- change.py
+++ change.py
@@ -1,28 +1,25 @@
 import subprocess
 import random
 import time
-from twisted.internet import task
-from twisted.internet import reactor
 
-# subprocess.call(["defaults", "write", "com.apple.Desktop", "background" ])
-images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg' , '6.jpg']
+# List of image names to be grabbed randomly and pushed to file location located in SCRIPT TODO: Find a cleaner way to do this
 
-timeout = 10.0
+images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg' , '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg', '28.jpg', '29.jpg', '30.jpg', '31.jpg', '32.jpg', '33.jpg', '34.jpg', '35.jpg', '36.jpg', '37.jpg', '38.jpg', '39.jpg', '40.jpg', '41.jpg', '42.jpg', '43.jpg', '44.jpg', '45.jpg', '46.jpg', '47.jpg', '48.jpg', '49.jpg', '50.jpg']
 
-SCRIPT = """
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/paulmartin/Desktop/Background/{}" '
-""".format(random.choice(images))
+# Function to be called in name main with while true to have loop on interval
 
 def set_desktop_background():
-    subprocess.Popen(SCRIPT, shell=True)
-    # time.sleep(10)
 
-# l = task.LoopingCall(set_desktop_background)
-# l.start(timeout)
+	SCRIPT = """
+	osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/paulmartin/change/Background/{}" '
+	""".format(random.choice(images))
 
-# reactor.run()
+	subprocess.Popen(SCRIPT, shell=True)
 
+# Do the thing
 if __name__ == '__main__':
-	# while True:
-	set_desktop_background()
-		# time.sleep(10)
+	while True:
+		set_desktop_background()
+		# 900 is 15 minutes. change as you desire.
+		time.sleep(900)
+
